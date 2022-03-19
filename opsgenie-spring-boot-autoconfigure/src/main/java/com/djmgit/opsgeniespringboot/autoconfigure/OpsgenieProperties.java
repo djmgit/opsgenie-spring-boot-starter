320abe739f2a474b9a3552afb4a6ac562eddc0b0
djmatlas <djmdeveloper060796@gmail.com>
2022-03-19 14:15:46 +0530

    Add more property holders

--- opsgenie-spring-boot-autoconfigure/src/main/java/com/djmgit/opsgeniespringboot/autoconfigure/OpsgenieProperties.java
+++ opsgenie-spring-boot-autoconfigure/src/main/java/com/djmgit/opsgeniespringboot/autoconfigure/OpsgenieProperties.java
@@ -1,12 +1,19 @@
 package com.djmgit.opsgeniespringboot.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
 @ConfigurationProperties(prefix = "opsgenie.springboot")
 public class OpsgenieProperties {
     
     private String opsgenieApiKey;
     private String opsgenieApiBase;
+    private String alertStatusCodes;
+    private String alertStatusClasses;
+    private String monitoredEndpoints;
+    private String ignoredEndpoints;
+    private String thresholdResponseTime;
+    private String responseTimeMonitoredEndpoints;
+    private String alertTags;
+    private String alertDetails;
 
     public String getOpsgenieApiKey() {
         return this.opsgenieApiKey;
