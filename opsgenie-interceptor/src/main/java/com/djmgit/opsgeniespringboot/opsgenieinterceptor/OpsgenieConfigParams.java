36189a3ad6299a5c91ec2fc48e2833c71ddca4f3
djmatlas <djmdeveloper060796@gmail.com>
2022-03-21 23:07:39 +0530

    Add property keys

--- opsgenie-interceptor/src/main/java/com/djmgit/opsgeniespringboot/opsgenieinterceptor/OpsgenieConfigParams.java
+++ opsgenie-interceptor/src/main/java/com/djmgit/opsgeniespringboot/opsgenieinterceptor/OpsgenieConfigParams.java
@@ -6,4 +6,17 @@ public class OpsgenieConfigParams {
     public static final String OPSGENIE_ALERT_STATUS_CLASSES = "opsgenie.alert.status.classes";
     public static final String OPSGENIE_ALERT_PRIORITY = "opsgenie.alert.priority";
     public static final String OPSGENIE_API_BASE = "opsgenie.api.base";
+    public static final String OPSGENIE_MONITORED_ENDPOINTS = "opsgenie.endpoints.monitored";
+    public static final String OPSGENIE_IGNORED_ENDPOINTS = "opsgenie.endpoints.ignored";
+    public static final String OPSGENIE_THRESHOLD_RESPONSE_TIME = "opsgenie.response.time.threshold";
+    public static final String OPSGENIE_RESPONSE_TIME_MONITORED_ENDPOINTS = "opsgenie.response.time.endpoints.monitored";
+    public static final String OPSGENIE_ALERT_TAGS = "opsgenie.alert.tags";
+    public static final String OPSGENIE_ALERT_DETAILS = "opsgenie.alert.details";
+    public static final String OPSGENIE_ALERT_ALIAS = "opsgenie.alert.alias";
+    public static final String OPSGENIE_ALERT_STATUS_ALIAS = "opsgenie.alert.alias.status";
+    public static final String OPSGENIE_ALERT_LATENCY_ALIAS = "opsgenie.alert.alias.latency";
+    public static final String OPSGENIE_ALERT_EXCEPTION_ALIAS = "opsgenie.alert.alias.exception";
+    public static final String OPSGENIE_RESPONDER = "opsgenie.alert.responder";
+    public static final String OPSGENIE_SERVICE_ID = "opsgenie.service.id";
+
 }
