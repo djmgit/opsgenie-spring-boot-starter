4b546a876c25b3afe138a10ba38df91f05a1ab16
djmatlas <djmdeveloper060796@gmail.com>
2022-03-22 23:20:15 +0530

    Create and use transformer populator

--- opsgenie-spring-boot-autoconfigure/src/main/java/com/djmgit/opsgeniespringboot/autoconfigure/Util.java
+++ opsgenie-spring-boot-autoconfigure/src/main/java/com/djmgit/opsgeniespringboot/autoconfigure/Util.java
@@ -24,27 +24,22 @@ public class Util {
         this.opsgenieProperties = opsgenieProperties;
     }
 
+    private void stringToArraylistTransformerPopulator(OpsgenieConfig opsgenieConfig, String propertyValString, String propertyKey) {
+        if (propertyValString == null) {
+            return;
+        }
+
+        ArrayList<String> propertyValList = new ArrayList<String>(Arrays.asList(propertyValString.trim().split(",")));
+        opsgenieConfig.put(propertyKey, propertyValList);
+    }
+
     public OpsgenieConfig getOpsgeneiConfig() {
         OpsgenieConfig opsgenieConfig = new OpsgenieConfig();
 
         String opsgenieApiKey = this.opsgenieProperties.getOpsgenieApiKey() == null ? System.getenv(OPSGENIE_API_KEY) : this.opsgenieProperties.getOpsgenieApiKey();
         String opsgenieApiBase = this.opsgenieProperties.getOpsgenieApiBase() == null ? "https://api.opsgenie.com" : this.opsgenieProperties.getOpsgenieApiBase();
-        String alertStatusCodes = this.opsgenieProperties.getAlertStatusCodes();
-
-        // transform alert status codes string into array array list of alert status codes
-        if (alertStatusCodes != null) {
-            ArrayList<String> alertStatusCodesList = new ArrayList<String>(Arrays.asList(alertStatusCodes.trim().split(",")));
-            opsgenieConfig.put(OPSGENIE_ALERT_STATUS_CODES, alertStatusCodesList);
-        }
-
-        String alertStatusClasses = this.opsgenieProperties.getAlertStatusClasses();
-
-        // trasnform alert status classes string into array list of classes
-        if (alertStatusClasses != null) {
-            ArrayList<String> alertStatusClassesList = new ArrayList<String>(Arrays.asList(alertStatusClasses.trim().split(",")));
-            opsgenieConfig.put(OPSGENIE_ALERT_STATUS_CLASSES, alertStatusClassesList);
-        }
-
+        stringToArraylistTransformerPopulator(opsgenieConfig, this.opsgenieProperties.getAlertStatusCodes(), OPSGENIE_ALERT_STATUS_CODES);
+        stringToArraylistTransformerPopulator(opsgenieConfig, this.opsgenieProperties.getAlertStatusClasses(), OPSGENIE_ALERT_STATUS_CLASSES);
         return opsgenieConfig;
     }
 }
