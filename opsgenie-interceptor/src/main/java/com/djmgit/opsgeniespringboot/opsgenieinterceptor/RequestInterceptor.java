430cc911c4c1603ba726a2d680907d4ff7b38b51
djmatlas <djmdeveloper060796@gmail.com>
2022-04-10 16:16:58 +0530

    Add opsgenie alert type enum

--- opsgenie-interceptor/src/main/java/com/djmgit/opsgeniespringboot/opsgenieinterceptor/RequestInterceptor.java
+++ opsgenie-interceptor/src/main/java/com/djmgit/opsgeniespringboot/opsgenieinterceptor/RequestInterceptor.java
@@ -1,6 +1,5 @@
 package com.djmgit.opsgeniespringboot.opsgenieinterceptor;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
---
+++ opsgenie-interceptor/src/main/java/com/djmgit/opsgeniespringboot/opsgenieinterceptor/models/OpsgenieAlertType.java
@@ -0,0 +1,8 @@
+package com.djmgit.opsgeniespringboot.opsgenieinterceptor.models;
+
+public enum OpsgenieAlertType {
+    STATUS_ALERT,
+    LATENCY_ALERT,
+    EXCEPTION,
+    MANUAL
+}
