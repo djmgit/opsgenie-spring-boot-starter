eb9f920b7e8b6f49b47bcfbbb00a18dd607c6ae6
djmatlas <djmdeveloper060796@gmail.com>
2022-04-21 23:12:14 +0530

    Enable back opsgenie autoconfigurer

--- opsgenie-spring-boot-autoconfigure/src/main/java/com/djmgit/opsgeniespringboot/autoconfigure/OpsgenieAutoconfiguration.java
+++ opsgenie-spring-boot-autoconfigure/src/main/java/com/djmgit/opsgeniespringboot/autoconfigure/OpsgenieAutoconfiguration.java
@@ -1,45 +1,45 @@
-//package com.djmgit.opsgeniespringboot.autoconfigure;
-//
-//import com.djmgit.opsgeniespringboot.opsgenieinterceptor.Intercept;
-//import com.djmgit.opsgeniespringboot.opsgenieinterceptor.Opsgenie;
-//import com.djmgit.opsgeniespringboot.opsgenieinterceptor.OpsgenieConfig;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import com.djmgit.opsgeniespringboot.opsgenieinterceptor.RequestInterceptor;
-//import static com.djmgit.opsgeniespringboot.opsgenieinterceptor.OpsgenieConfigParams.*;
-//
-//
-//@Configuration
-//@ConditionalOnClass(Opsgenie.class)
-//@EnableConfigurationProperties(OpsgenieProperties.class)
-//public class OpsgenieAutoconfiguration {
-//
-    //@Autowired
-    //private OpsgenieProperties opsgenieProperties;
-//    
-    //@Bean
-    //@ConditionalOnMissingBean
-    //public Intercept interceptor() {
-        //return new Intercept();
-    //}
-//
-    //@Bean
-    //@ConditionalOnMissingBean
-    //public OpsgenieConfig opsgenieConfig() {
-        //OpsgenieConfig opsgenieConfig = new Util(this.opsgenieProperties).getOpsgeneiConfig();
-        //return opsgenieConfig;
-    //}
-//
-    //@Bean
-    //@ConditionalOnMissingBean
-    //public Opsgenie opsgenie(OpsgenieConfig opsgenieConfig) {
-//
-        //return new Opsgenie(opsgenieConfig);
-    //}
-//}
+package com.djmgit.opsgeniespringboot.autoconfigure;
+
+import com.djmgit.opsgeniespringboot.opsgenieinterceptor.Intercept;
+import com.djmgit.opsgeniespringboot.opsgenieinterceptor.Opsgenie;
+import com.djmgit.opsgeniespringboot.opsgenieinterceptor.OpsgenieConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.djmgit.opsgeniespringboot.opsgenieinterceptor.RequestInterceptor;
+import static com.djmgit.opsgeniespringboot.opsgenieinterceptor.OpsgenieConfigParams.*;
+
+
+@Configuration
+@ConditionalOnClass(Opsgenie.class)
+@EnableConfigurationProperties(OpsgenieProperties.class)
+public class OpsgenieAutoconfiguration {
+
+    @Autowired
+    private OpsgenieProperties opsgenieProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Intercept interceptor() {
+        return new Intercept();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public OpsgenieConfig opsgenieConfig() {
+        OpsgenieConfig opsgenieConfig = new Util(this.opsgenieProperties).getOpsgeneiConfig();
+        return opsgenieConfig;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Opsgenie opsgenie(OpsgenieConfig opsgenieConfig) {
+
+        return new Opsgenie(opsgenieConfig);
+    }
+}
